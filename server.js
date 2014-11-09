5cdf80e68550a666bdcda3f5916df3c3c7e8941d
CTownsdin <Townsdin@gmail.com>
2014-11-09 11:21:20 -0800

    Change files

--- server.js
+++ server.js
@@ -4,14 +4,10 @@ var bodyparser = require('body-parser');
 var app = express();
 require('./routes/notes_routes')(app);
 
-var url = process.env.MONGOHQ_URL || process.env.MONGOLAB_URI;
+var url = process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/notes-development';
 mongoose.connect(url);
-
 app.use(bodyparser.json());
-
-
 app.set('port', process.env.PORT || 3000);
-
 
 app.listen(app.get('port'), function() {
   console.log('server running on port: %d', app.get('port'));
